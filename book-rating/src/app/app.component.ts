a5836e0417285caf9cac60d4de6a4c10bae01f69
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-02-08 10:00:05 +0100

    Fun with Observables

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,16 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'br-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Book Rating!';
 
-  /*constructor() {
-    setTimeout(() => {
-      this.title = 'HALLO';
-    }, 2000);
-  }*/
+  ngOnInit() {
+
+
+    const myObserver = {
+      next: value => console.log(value),
+      error: err => console.error('ERR', err),
+      complete: () => console.log('Complete')
+    };
+
+
+    const myObs = new Observable(observer => {
+      setTimeout(() => observer.next('A'), 2000);
+
+      observer.next('B');
+      observer.next('C');
+
+      setTimeout(() => observer.complete(), 3000);
+      setTimeout(() => {
+        observer.next('HALLO');
+        console.log('???');
+      }, 4000);
+    });
+
+    const sub = myObs.subscribe(myObserver);
+
+    setTimeout(() => sub.unsubscribe(), 1000);
+
+
+    
+
+
+    
+
+
+    function observable(observer) {
+      setTimeout(() => observer.next('A'), 2000);
+
+      observer.next('B');
+      observer.next('C');
+
+      setTimeout(() => observer.complete(), 3000);
+      setTimeout(() => observer.next('HALLO'), 4000);
+
+    }
+
+
+   //  observable(myObserver);
+
+
+
+
+
+
+  }
 }
